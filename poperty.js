e9f57fa2824c4441345c27c4a4340e13a17bba07
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-04 23:44:25 +0600

    Added get & set object properties

---
+++ poperty.js
@@ -0,0 +1,25 @@
+const person = {
+ name: 'sodor uddin',
+ age: 25,
+ profession: 'developer',
+ salary: 25000,
+ married: true,
+ 'fev places': ['bandorban', 'saintmartin', 'kuakata']
+}
+
+// console.log(person);
+
+//dot notation
+// dot symble diye object ar value access kora
+// console.log(person.salary)
+const income = person.age;
+// console.log(income)
+
+
+// braket Notation
+// taird bracket diya access kora
+// console.log(person['name'])
+const boyos = person['age']
+// console.log(boyos)
+
+console.log(person['fev places'])
---
+++ value.js
@@ -0,0 +1,12 @@
+const person = {
+ name: 'sodor uddin',
+ age: 25,
+ profession: 'developer',
+ salary: 25000,
+ married: true,
+ 'fev places': ['bandorban', 'saintmartin', 'kuakata']
+}
+person.salary = 400000;
+person['age'] = 35;
+person['fev places'] = ['maldivs', 'bali', 'india']
+console.log(person)
