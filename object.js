29928588da5bc8035491a6cd827a74809df4938f
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-04 23:08:03 +0600

    Introduction to Objects

---
+++ object.js
@@ -0,0 +1,32 @@
+const age = 21;
+const school = 'rifle square';
+const ispassed = true;
+// let isDevloper;
+
+const Subjects = ['Bangla', 'English', 'Physics', 'Math'];
+const Bottle = ['white', 45];
+const BottleColor = 'white';
+const BottlePrice = 45;
+const BottleDrink = 'water';
+
+// console.log(isDevloper);
+
+//  Object
+const bottle = {
+ brand: 'apple',
+ price: 45,
+ color: 'white',
+ isClean: false
+}
+
+const Subject = {
+ name: 'biology',
+ teacher: 'resheda mam',
+ examDate: '30 Feb',
+ chapters: ['first', 'second', 'thired'],
+ exams: {
+  name: 'final Exam',
+  marks: 100
+ }
+}
+console.log(Subject);
