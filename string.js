4b1709a612f98dd733c2e2cd826463fa572fc522
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-04 17:12:07 +0600

    Added String Example

---
+++ string.js
@@ -0,0 +1,24 @@
+const country = 'bangladesh';
+const division = "Dinajpur";
+const districk = `Khansama`;
+
+// const thana = new String ('Demra');
+
+// console.log(typeof districk);
+
+
+const numbers = [45, 98, 85, 75, 65, 32];
+// console.log(numbers.length);
+// console.log(numbers[2])
+// numbers[1] = 11;
+// console.log(numbers)
+
+// String is imutable --> not changable
+// Arry is mutable -----> you can change the arry elements
+
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[3]);
+console.log(capital);
+capital[3] = 'k';
+console.log(capital);
