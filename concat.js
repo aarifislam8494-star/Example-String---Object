d925e8e41fa46ab511c1b1efe9e3f0f617e7e07f
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-04 20:49:23 +0600

    String Slice, Join, Concat and includes

---
+++ concat.js
@@ -0,0 +1,14 @@
+const frist = 'arif';
+const last = 'Islam';
+
+
+// const fullName =frist + last;
+const fullName =frist + ' ' + last;
+// console.log(fullName);
+
+const fullName2 = frist.concat(' ').concat(last);
+console.log(fullName2);
+
+// includes 
+
+console.log(last.includes('x'));
