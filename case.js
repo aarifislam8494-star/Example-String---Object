68ece918a5cb9d9c62a481cf30f455e0cc0c61a0
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-04 18:06:26 +0600

    added Case---> UpperCase/LowerCase

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = 'RAJ UK Ultra Model School';
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+// Uppercase = ABCD
+// Lowercase = abcd
+const Subject = 'Chemistry';
+const Book = 'chemIsTry';
+
+if(Subject.toLowerCase() === Book.toLowerCase()){
+ console.log('I am reading book aibar porikkhai fatai felmu');
+}
+else{
+ console.log('hudai hudai prista ultai ar vat khawer jonno');
+}
+
+const drink =  ' water';
+const liquid = 'water '
+if(drink.trim() === liquid.trim()){
+ console.log('pani er opor name jibon');
+}
+else{
+ console.log('somudre pani ace khaite pari na')
+} 
+
